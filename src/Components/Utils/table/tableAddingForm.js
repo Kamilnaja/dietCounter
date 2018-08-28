4cfd3d16d39b9efdeca152071d21752a48c76d61
kamilnaja <kamilnaja@gmail.com>
2018-08-28 21:05:51 +0200

    added some styles to table

--- src/Components/Utils/table/tableAddingForm.js
+++ src/Components/Utils/table/tableAddingForm.js
@@ -7,38 +7,35 @@ class TableAddingForm extends Component {
     }
 
     render() {
-        return (<tr>
-            <td>
-            </td>
-            <td>
-                <input
-                    type="text"
-                    placeholder="name"
-                    value={this.props.name}
-                    onChange={(e) => this.props.handleChange(e, 'name')} />
-            </td>
-            <td>
-                <input type="text"
-                    placeholder="kcal"
-                    value={this.props.kcal}
-                    onChange={(e) => this.props.handleChange(e, 'kcal')} />
-            </td>
-            <td>
-                <input
-                    type="text"
-                    placeholder="category"
-                    value={this.props.category}
-                    onChange={(e) => this.props.handleChange(e, 'category')} />
-            </td>
-            <td>
-                <button onClick={(e) => this.props.handleSubmit(e)}>
-                    Save
-                </button>
-                <button>
-                    Cancel
-                </button>
-            </td>
-        </tr>
+        return (
+            <tr className='tableInputRow'>
+                <td>
+                </td>
+                <td>
+                    <input
+                        type="text"
+                        placeholder="name"
+                        value={this.props.name}
+                        onChange={(e) => this.props.handleChange(e, 'name')} />
+                </td>
+                <td>
+                    <input type="text"
+                        placeholder="kcal"
+                        value={this.props.kcal}
+                        onChange={(e) => this.props.handleChange(e, 'kcal')} />
+                </td>
+                <td>
+                    <input
+                        type="text"
+                        placeholder="category"
+                        value={this.props.category}
+                        onChange={(e) => this.props.handleChange(e, 'category')} />
+                </td>
+                <td>
+                    <button onClick={(e) => this.props.handleSubmit(e)}>Save</button>
+                    <button>Cancel</button>
+                </td>
+            </tr>
         );
     }
 }
